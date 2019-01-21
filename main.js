84a7ca0c67d66fb4133a75b191e06094ed55cfe9
Niklas Wollburg <nikwol@online.de>
2019-01-21 14:33:26 +0100

    Überlegungen

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ const path = require('path');
 const url = require('url');
 
 const {
-  app,
-  BrowserWindow
+     app,
+     BrowserWindow
 } = electron;
 require('electron-debug')({
-  showDevTools: true
+     showDevTools: true
 });
 let mainWindow;
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({});
-  mainWindow.setMenu(null);
-  mainWindow.maximize();
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, '/mainWindow.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+app.on('ready', function () {
+     mainWindow = new BrowserWindow({});
+     mainWindow.setMenu(null);
+     mainWindow.maximize();
+     mainWindow.loadURL(url.format({
+          pathname: path.join(__dirname, '/mainWindow.html'),
+          protocol: 'file:',
+          slashes: true
+     }));
 });

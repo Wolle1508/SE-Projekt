56b8f8cc685074f946d1a1d126f645e17754186e
Niklas Wollburg <nikwol@online.de>
2019-01-25 18:59:03 +0100

    pause function rewriten; styling

--- ablauf.js
+++ ablauf.js
@@ -1,7 +1,7 @@
 var daten;
 var pause = false;
 var index = 1;
-var speed = 5000
+var speed = 1000
 
 window.onload = function () {
      fetch('daten.json')
@@ -12,24 +12,31 @@ window.onload = function () {
                daten = myJson;
           });
      document.getElementById("start").addEventListener("click", function () {
-          document.getElementById("clear").disabled = true;
-          ablauf();
-          // document.getElementById("clear").disabled = false;
-     });
-     document.getElementById("pause").addEventListener("click", function () {
-          document.getElementById("clear").disabled = false;
-          console.log("pause");
-          pause = true;
+          var startButton = document.getElementById("start");
+          var clearbutton = document.getElementById("clear");
+          if (startButton.value == "start") {
+               clearbutton.disabled = true;
+               pause = false;
+               ablauf();
+               startButton.value = "pause";
+               startButton.innerHTML = "Pause";
+          }else{
+               startButton.value = "start";
+               startButton.innerHTML = "Start";
+               clearbutton.disabled = false;
+               pause = true;
+          }
      });
      document.getElementById("clear").addEventListener("click", function () {
           clearMap();
      });
-     document.getElementById("speed").addEventListener("change", function(){
+     document.getElementById("speed").addEventListener("change", function () {
           speed = document.getElementById("speed").value * 1000
      });
 }
 
 function ablauf() {
+     if(index != 1) index++;
      pause = false;
      var textfield = document.getElementById("info");
      (function theLoop(i) {
@@ -40,18 +47,19 @@ function ablauf() {
                textfield.append("\n");
                if (index != Object.keys(daten).length && !pause) {
                     index += 1;
-                    theLoop(index); 
+                    theLoop(index);
                }
           }, speed);
      })(index);
 }
 
-function clearMap(){
+function clearMap() {
      var cells = document.getElementsByClassName("land-infected");
      for (const cell in cells) {
           if (cells.hasOwnProperty(cell)) {
                cells[cell].className = "land";
           }
      }
+     document.getElementById("info").innerHTML = "";
      index = 1;
 }
